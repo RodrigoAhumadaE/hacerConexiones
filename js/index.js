05990df81f8729fb0482031e2e3376a49c8b2e9f
Rodrigo Enrique Ahumada Espinoza <rod.ahumada@duocuc.cl>
2023-09-20 12:09:32 -0300

    Ejercicio terminado

--- js/index.js
+++ js/index.js
@@ -1,9 +1,38 @@
 function eliminarConexion(element){
-  let elementcontacto = element.closest('.user-contact');
+  let elementContacto = element.closest('.user-contact');
   let elementNumContactos = element.closest('.contactos-pendientes').querySelector('.num-contact');
   let numContactos = elementNumContactos.innerText;
   numContactos--;
   elementNumContactos.textContent = numContactos;
-  elementcontacto.remove();
+  elementContacto.remove();
+}
 
+function agregarConexion(element){
+  let elementContactosAceptados = document.querySelector('.contactos-aceptados');
+  let numContactosAceptados = elementContactosAceptados.querySelector('.num-contact').innerText;
+  numContactosAceptados++;
+  elementContactosAceptados.querySelector('.num-contact').textContent = numContactosAceptados;
+  let listaContactosAceptados = elementContactosAceptados.querySelector('.cuerpo');
+  let elementImgContactoAgregar = element.closest('.user-contact');
+  let imgContactoAgregar = elementImgContactoAgregar.querySelector('img');
+  let nombreContactoAgregar = elementImgContactoAgregar.querySelector('h4').innerText;
+  console.log(imgContactoAgregar.src); 
+
+  listaContactosAceptados.innerHTML += `
+  <div class="contact">
+    <img src="${imgContactoAgregar.src}" alt="${imgContactoAgregar.alt}">
+    <h4>${nombreContactoAgregar}</h4>
+  </div>`;
+  
+  eliminarConexion(element);
+}
+
+function cambiarNombreUsuario(element){
+  let elementNombreUsuario = element.closest('.info-card').querySelector('.nombre-perfil');
+  let nombreUsuario = elementNombreUsuario.innerText;
+  if(nombreUsuario === 'Jane Doe'){
+    elementNombreUsuario.textContent = 'Annette Cardenas';
+  }else{
+    elementNombreUsuario.textContent = 'Jane Doe';
+  }
 }
